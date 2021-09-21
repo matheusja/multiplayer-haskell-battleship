741de4c1382f5125d667e76fe35808295ac126b2
matheus <matheusjahnke@hotmail.com>
2021-09-21 17:46:18 -0300

    Client inits with command line args

--- client/Main.hs
+++ client/Main.hs
@@ -9,19 +9,53 @@ import System.Environment
 import Data.Maybe
 import Data.List
 
+import qualified System.IO as I_O
+import qualified System.Console.ANSI as Console
+{-# ANN module "HLint: ignore Use camelCase" #-}
 
+start = do
+  I_O.hSetBuffering I_O.stdin I_O.NoBuffering
+  I_O.hSetEcho      I_O.stdin False
+  Console.hideCursor
+
+exit = do
+  I_O.hSetEcho      I_O.stdin  True
+  Console.showCursor
+
+
+parse_args args = case args of
+  ["help"] -> Nothing
+  [ip] -> Just ("127.0.0.1", "3000", "lobby")
+  [ip, port] -> Just (ip, port, "lobby")
+  [ip, port, lobby] -> Just (ip, port, "join " ++ lobby)
+  [] -> Just ("127.0.0.1", "3000", "lobby")
+  _ -> Nothing 
+   
+print_usage = do
+  putStrLn "USAGE:"
+  putStrLn "<program> <ip> <port> <lobby code>; will join lobby assigned to code"
+  putStrLn "<program> <ip> <port>; will create lobby"
+  putStrLn "<program> <ip>; port = 3000"
+  putStrLn "<program>; "
+  
 main :: IO ()
-main = runTCPClient "127.0.0.1" "3000" $ \s -> do
-    args <- getArgs
-    let message = maybe "lobby" (("join " ++ ) . fst) $ uncons args
-    NS.sendAll s $ C.string_to_utf8 message
-    msg <- NS.recv s 1024
-    putStrLn $ "Received: " ++ C.string_from_utf8 msg
-    _ <- getLine -- evitar sair
-    msg <- NS.recv s 1024
-    putStrLn $ "Received: " ++ C.string_from_utf8 msg
-    _ <- getLine -- evitar sair
-    return ()
+main = do
+  args <- getArgs
+  case parse_args args of
+    Nothing -> print_usage
+    Just (add, port, message) -> E.bracket_ start exit (connection add port message)
+
+  
+  
+connection add port message = runTCPClient add port $ \s -> (do
+  NS.sendAll s $ C.string_to_utf8 message
+  msg <- NS.recv s 1024
+  putStrLn $ "Received: " ++ C.string_from_utf8 msg
+  _ <- getLine -- evitar sair
+  msg <- NS.recv s 1024
+  putStrLn $ "Received: " ++ C.string_from_utf8 msg
+  _ <- getLine -- evitar sair
+  return ())
 
 -- from the "network-run" package.
 runTCPClient :: HostName -> ServiceName -> (Socket -> IO a) -> IO a
